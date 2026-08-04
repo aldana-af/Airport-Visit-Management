afee0f5c91813ab84ca28a5528a782ba7c1df632
aldana-af <danafelemban@gmail.com>
2026-08-04 13:21:05 +0300

    Initial Project Creation

---
+++ AirportVisitSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AirportVisitSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AirportVisitSystem/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirportVisitSystem.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+    }
+}
